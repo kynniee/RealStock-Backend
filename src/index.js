da744e490704147c5562f949d6453cdf4d3ead15
kynniee <phambaoky@gmail.com>
2024-07-11 22:49:55 +0700

    product

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001
 
+app.use(express.json({limit:'50mb'}))
+app.use(express.urlencoded({limit:'50mb'}))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(cookieParser())
